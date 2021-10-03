f6f322da09155973df32f895edaf15f3e6d4260b
Vsevolod <Vsevolod-Shustov@users.noreply.github.com>
2021-10-03 17:51:40 +0300

    crash restore

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar'
 
+import './_variables.scss';
 import './Header.scss';
 import Logo from '../public/images/logo.svg'
 
@@ -11,6 +12,9 @@ function Header(){
         <img src={Logo} className="logo" />
       </a>
       <Navbar />
+      <a href="http://example.com" target="_blank" rel="noreferrer" className="nav-item login-button">Login</a>
+      <a href="http://example.com" target="_blank" rel="noreferrer" className="nav-item signup-button">Sign Up Free</a>
+      <button className="navbar-mobile-toggle mobile">button</button>
     </header>
   )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,8 +2,6 @@ import React, {useState} from 'react';
 
 import './Navbar.scss';
 
-//const links = [];
-
 function Navbar(){
   const [isOpen, setIsOpen] = useState(false);
   const handleControllerClick = () => {
@@ -16,18 +14,18 @@ function Navbar(){
   }
   return (
     <nav id="navbar">
-      <div className="dropdown-controller" onClick={handleControllerClick}>
+      <div className="dropdown-controller nav-item" onClick={handleControllerClick}>
       Features <span className="dropdown-status">{isOpen ? "^" : "v"}</span>
 
         {isOpen && <div className="dropdown-content" onClick={handleContentClick}>
-        <a href="http://example.com">Rank Tracker</a>
-        <a href="http://example.com">Reporting</a>
-        <a href="http://example.com">Site Audit</a>
+        <a href="http://example.com" target="_blank" rel="noreferrer" className="nav-item">Rank Tracker</a>
+        <a href="http://example.com" target="_blank" rel="noreferrer" className="nav-item">Reporting</a>
+        <a href="http://example.com" target="_blank" rel="noreferrer" className="nav-item">Site Audit</a>
         </div>}
       </div>
-      <a href="http://example.com">Enterprise</a>
-      <a href="http://example.com">Pricing</a>
-      <a href="http://example.com">Blog</a>
+      <a href="http://example.com" target="_blank" rel="noreferrer" className="nav-item">Enterprise</a>
+      <a href="http://example.com" target="_blank" rel="noreferrer" className="nav-item">Pricing</a>
+      <a href="http://example.com" target="_blank" rel="noreferrer" className="nav-item">Blog</a>
     </nav>
   )
 }
